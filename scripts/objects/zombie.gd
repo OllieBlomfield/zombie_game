a1ceebfb9fd5b0d2ca0eabe2be5299db59403366
Shadowking8090 <Shadowking8090@gmail.com>
2026-06-10 07:57:02 -0400

    Enemy navigation

--- scripts/objects/zombie.gd
+++ scripts/objects/zombie.gd
@@ -9,33 +9,41 @@ extends CharacterBody2D
 
 @export var speed: int
 @export var jump_height: int
+@export var gravity: float = 200
 
+var breadcrumbs: Array = []
 
 
 enum States{CHASING, ATTACKING, JUMPING, DEAD}
 var state: States = States.CHASING
 
 func _ready() -> void:
-	pass
+	player = get_tree().get_first_node_in_group("player")
+	breadcrumbs.append(player.global_position)
 	
 func _physics_process(delta: float) -> void:
-	update_state()
-
+	velocity.y += gravity * delta
+	update_state(delta)
 func move():
 	pass
 	
 func attack():
 	pass 
 	
-func chase():
-	pass
-	#var dir = sign(player.global_position.x - global_position.x)
-	#velocity.x = dir * speed
-	#if is_on_wall():
-		#jump()
-	#if player.global_position.y < global_position.y - 50:
-		#jump()
-	#move_and_slide()
+func chase(delta):
+	if breadcrumbs.is_empty():
+		return
+	if global_position.distance_to(breadcrumbs[0]) < 2.0:
+		breadcrumbs.pop_front()
+		if breadcrumbs.is_empty():
+			return
+	var dir = global_position.direction_to(breadcrumbs[0]) * speed
+	velocity.x = dir.x
+	if is_on_wall():
+		jump()
+	if player.global_position.y < global_position.y - 50:
+		jump()
+	move_and_slide()
 	
 func jump():
 	if is_on_floor():
@@ -44,11 +52,18 @@ func jump():
 func die():
 	queue_free()
 
-func update_state():
+func update_state(delta):
 	match state:
 		States.CHASING:
-			chase()
+			chase(delta)
 		States.ATTACKING:
 			pass
 		States.DEAD:
 			pass
+
+
+func _on_bread_crumb_timer_timeout() -> void:
+	breadcrumbs.append(player.global_position)
+	if breadcrumbs.size() > 10:
+		breadcrumbs.pop_front()
+	print(breadcrumbs[0])
