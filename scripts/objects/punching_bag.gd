935eafc12d06f9fa3abc314b24a8afd75467396d
OllieBlomfield <ollieb@unc.edu>
2026-06-10 15:34:04 +0200

    added flash shader to punching bag

--- scripts/objects/punching_bag.gd
+++ scripts/objects/punching_bag.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @onready var hurt_box: HurtBox = $HurtBox
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var flash: bool = false
 
 func _ready() -> void:
 	hurt_box.received_damage.connect(_on_received_damage)
@@ -9,6 +12,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	#set_instance_shader_parameter("flash_modifier",0.0)
+	if flash:
+		sprite_2d.set_instance_shader_parameter("flash_modifier",1.0)
+		flash = false
+	else:
+		sprite_2d.set_instance_shader_parameter("flash_modifier",0.0)
 
-func _on_received_damage():
-	set_instance_shader_parameter("flash_modifier",1.0)
+func _on_received_damage(damage):
+	print("IM HIT")
+	flash = true
+	#sprite_2d.set_instance_shader_parameter("flash_modifier",1.0)
