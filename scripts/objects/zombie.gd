ccee04e727c6189f1cc9150861abb83e614465ef
Shadowking8090 <Shadowking8090@gmail.com>
2026-06-10 07:04:24 -0400

    enemy started

--- scripts/objects/zombie.gd
+++ scripts/objects/zombie.gd
@@ -1,12 +1,54 @@
-class_name Zombie
-extends Enemy
+class_name Enemy
+extends CharacterBody2D
+
+@export var player: CharacterBody2D
+
+@export var health: Health
+@export var hitbox: HitBox
+@export var hurtbox: HurtBox
+
+@export var speed: int
+@export var jump_height: int
 
 
-# Called when the node enters the scene tree for the first time.
+
+enum States{CHASING, ATTACKING, JUMPING, DEAD}
+var state: States = States.CHASING
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	update_state()
+
+func move():
+	pass
+	
+func attack():
+	pass 
+	
+func chase():
+	pass
+	#var dir = sign(player.global_position.x - global_position.x)
+	#velocity.x = dir * speed
+	#if is_on_wall():
+		#jump()
+	#if player.global_position.y < global_position.y - 50:
+		#jump()
+	#move_and_slide()
+	
+func jump():
+	if is_on_floor():
+		velocity.y = jump_height
+
+func die():
+	queue_free()
+
+func update_state():
+	match state:
+		States.CHASING:
+			chase()
+		States.ATTACKING:
+			pass
+		States.DEAD:
+			pass
