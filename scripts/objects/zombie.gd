8cf891ad7f7abf92b917e22776bf34b4b34a614e
Shadowking8090 <Shadowking8090@gmail.com>
2026-06-11 07:57:12 -0400

    Zombie DropDown and tileset

--- scripts/objects/zombie.gd
+++ scripts/objects/zombie.gd
@@ -92,6 +92,10 @@ func damaged_received(context: HitContext):
 func die() -> void:
 	queue_free()
 
-func _on_jump_zone_detector_area_entered(area: Area2D) -> void:
+func _on_zone_detector_area_entered(area: Area2D) -> void:
 	if area is JumpNode and abs(player.global_position.y - global_position.y) > 5:
 		jump(player.global_position.y)
+	elif area is DropNode and player.global_position.y > global_position.y + 3:
+		await get_tree().create_timer(0.1).timeout
+		print("down")
+		position.y += 5
