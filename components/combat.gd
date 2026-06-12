9ed1d2d5da37cc1b49b5a03887af99239489b1f1
OllieBlomfield <ollieb@unc.edu>
2026-06-12 18:42:12 +0200

    added weapon flipping when facing other direction

--- components/combat.gd
+++ components/combat.gd
@@ -1,7 +1,11 @@
 class_name Combat
 extends Node
 
+@export var player: Player #not good practice but need to get facing direction somehow
 @export var weapon_list: Array[Weapon] #would later need to add to this list from code
+@export var right_weapon_position: Marker2D
+@export var left_weapon_position: Marker2D
+
 var _current_weapon: Weapon
 var _current_weapon_index: int = 0
 
@@ -9,6 +13,14 @@ signal attack_finished
 
 func _ready() -> void:
 	set_weapon(0)
+
+func _process(delta: float) -> void:
+	if player.facing_direction == 1:
+		_current_weapon.scale.x = 1
+		_current_weapon.global_position = right_weapon_position.global_position
+	else:
+		_current_weapon.scale.x = -1
+		_current_weapon.global_position = left_weapon_position.global_position
 
 func attack(direction: float):
 	_current_weapon.flip_h = direction < 0
