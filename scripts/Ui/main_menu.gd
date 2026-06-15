ac28247adfd3828531065eb25f13f0e8475d8e87
Shadowking8090 <Shadowking8090@gmail.com>
2026-06-15 12:10:06 -0400

    Bug fixes part 2

--- scripts/Ui/main_menu.gd
+++ scripts/Ui/main_menu.gd
@@ -3,6 +3,7 @@ extends Node2D
 const WORLD = preload("uid://dnocukpr5f4uf")
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var start_music: AudioStreamPlayer2D = $StartMusic
 
 func _ready() -> void:
 	animation_player.animation_finished.connect(transition_to_level)
@@ -10,6 +11,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
+		start_music.play()
+		Input.start_joy_vibration(0, .3, .4, 3)
+		await get_tree().create_timer(.5).timeout
 		animation_player.play("transition_to_level")
 		#get_tree().change_scene_to_packed(WORLD)
 		
